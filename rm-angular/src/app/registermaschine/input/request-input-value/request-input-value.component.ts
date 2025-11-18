83e78ac8436d30b880b9abc458d0708f5386af25
Andreas Pazureck <andreas@pazureck.de>
2025-11-18 23:28:14 +0100

    input using return is now working

--- rm-angular/src/app/registermaschine/input/request-input-value/request-input-value.component.ts
+++ rm-angular/src/app/registermaschine/input/request-input-value/request-input-value.component.ts
@@ -1,16 +1,33 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MAT_DIALOG_DATA, MatDialogTitle, MatDialogModule } from '@angular/material/dialog';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogTitle,
+  MatDialogModule,
+  MatDialogRef,
+} from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'rma-request-input-value',
-  imports: [MatDialogTitle, MatDialogModule, MatFormFieldModule, FormsModule, MatInputModule, MatButtonModule],
+  imports: [
+    MatDialogTitle,
+    MatDialogModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: './request-input-value.component.html',
   styleUrl: './request-input-value.component.scss',
 })
 export class RequestInputValueComponent {
+  #dialogRef = inject(MatDialogRef<RequestInputValueComponent>);
   readonly value = signal<number>(inject(MAT_DIALOG_DATA));
+
+  closeDialog(value: number) {
+    this.#dialogRef.close(value);
+  }
 }
